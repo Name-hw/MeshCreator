f15d6354db6370898cb541bd872efba4b13a7da9
NameSpace <sieon2779@gmail.com>
2024-08-25 19:19:23 +0900

    Removed MeshLoader

--- src/MeshCreatorPlugin/StarterActor/StarterScript.server.lua
+++ src/MeshCreatorPlugin/StarterActor/StarterScript.server.lua
@@ -9,15 +9,15 @@ local function CheckBetaFeatureEnabled()
 end
 
 Root.ScriptActor.MeshCreatorScript.Enabled = false
-Root.MeshLoaderActor.MeshLoaderHandler.Enabled = false
+--Root.MeshLoaderActor.MeshLoaderHandler.Enabled = false
 
 print(s .. Version)
 
 if pcall(CheckBetaFeatureEnabled) then
 	Root.ScriptActor.MeshCreatorScript.Enabled = true
-	Root.MeshLoaderActor.MeshLoaderHandler.Enabled = true
+	--Root.MeshLoaderActor.MeshLoaderHandler.Enabled = true
 else
 	Root.ScriptActor.MeshCreatorScript.Enabled = false
-	Root.MeshLoaderActor.MeshLoaderHandler.Enabled = false
+	--Root.MeshLoaderActor.MeshLoaderHandler.Enabled = false
 	warn(s .. "Please enable EditableImage and EditableMesh in the beta features.")
 end
